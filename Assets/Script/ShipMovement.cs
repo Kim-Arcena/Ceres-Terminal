3ff157768ade22d9e9bc75f59b90c416a0bed3c2
Kristan Jay Gabay <ktgabay@up.edu.ph>
2024-04-17 15:40:16 +0800

    added shooting

--- Assets/Script/ShipMovement.cs
+++ Assets/Script/ShipMovement.cs
@@ -4,32 +4,68 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotateSpeed;
+    [Header("Ship Movement")]
+    [SerializeField] private float maxMoveSpeed = 10f;
+    [SerializeField] private float rotateSpeed = 180f;
+    [SerializeField] private float acceleration = 10f;
 
-    void Update()
+    [Header("Ship Components")]
+    [SerializeField] private Transform spawnBullet;
+    [SerializeField] private Rigidbody bulletPrefab;
+    [SerializeField] private float bulletSpeed = 20f;
+
+
+    private Rigidbody shipRb;
+    private bool isAlive = true;
+    private bool isAccelerating = false;
+    
+
+    private void Start(){
+        shipRb = GetComponent<Rigidbody>();
+
+    }
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (isAlive)
         {
-            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+            ShipAccelaration();
+            ShipRotation();
+            ShipShoot();
         }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
+    }
+    private void ShipAccelaration()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
         {
-            Vector3 currentRotation = transform.rotation.eulerAngles;
-
-            currentRotation.y -= rotateSpeed * Time.deltaTime;
-
-            transform.rotation = Quaternion.Euler(currentRotation);
+            shipRb.AddForce(transform.up * acceleration);
+            shipRb.velocity = Vector3.ClampMagnitude(shipRb.velocity, maxMoveSpeed);
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
+    }
+    private void ShipRotation()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
-            Vector3 currentRotation = transform.rotation.eulerAngles;
-
-            currentRotation.y += rotateSpeed * Time.deltaTime;
-
-            transform.rotation = Quaternion.Euler(currentRotation);
+            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * -rotateSpeed * Time.deltaTime);
+        }
+    }
+    private void ShipShoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shoot");
+            Rigidbody bullet = Instantiate(bulletPrefab, spawnBullet.position, Quaternion.identity);
+            Vector3 shipVelocity = shipRb.velocity;
+            Vector3 shipDirection = transform.rotation * Vector3.up;
+            float shipSpeed = Vector3.Dot(shipVelocity, shipDirection);
+            if(shipSpeed <0){
+                shipSpeed = 0;
+            }
+            bullet.velocity = shipSpeed * shipDirection;
+            bullet.AddForce(spawnBullet.up * bulletSpeed, ForceMode.Impulse);
         }
     }
 
