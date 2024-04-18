3e183322a043288e1b96b6f2b7ae6f471fe7b3ef
Divine <divinelavente@gmail.com>
2024-04-18 16:04:42 +0800

    created blackhole spawner
    > set up blackhole behavior

--- Assets/Script/BlackholeBehavior.cs
+++ Assets/Script/BlackholeBehavior.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BlackholeBehavior : MonoBehaviour
 {
-    [SerializeField] public float rotationSpeed = 30f;
+    [SerializeField] private float rotationSpeed = 150f;
+    [SerializeField] private float scaledRotation;
+    [SerializeField] private float delaySeconds = 2.0f;
+    private bool destructionScheduled = false;
+    private float delayStartTime;
     void Start()
     {
-        
+        Transform parentTransform = transform.parent;
+        scaledRotation = rotationSpeed / parentTransform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward, scaledRotation * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Transform parentTransform = transform.parent;
+            float delaySeconds = (parentTransform.localScale.x * -0.6f) + 3.9f;
+            print(parentTransform.localScale.x);
+            print(delaySeconds);
+            Destroy(collision.gameObject, delaySeconds);
+        }
     }
 }
