1c39a82eddc991b561e5a6e1ce55e51c84b07fd4
Kristan Jay Gabay <ktgabay@up.edu.ph>
2024-04-18 17:26:48 +0800

    added asteroid split feature
    test

--- Assets/Script/Asteroids.cs
+++ Assets/Script/Asteroids.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Asteroids : MonoBehaviour
 {
     [SerializeField] private float size;
-<<<<<<< Updated upstream
-
-=======
     private string asteroidType;
     [SerializeField] private ParticleSystem destroyFx;
     [SerializeField] private Asteroids[] mediumAsteroidPrefabs;
@@ -15,14 +12,13 @@ public class Asteroids : MonoBehaviour
 
 
     private int hits = 0;
->>>>>>> Stashed changes
     public GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         // transform.localScale = 0.5f * size * Vector3.one;
-
+        asteroidType = gameObject.name;
         Rigidbody rb = GetComponent<Rigidbody>();
 
         float directionX = Random.Range(-1f, 1f);
@@ -36,8 +32,6 @@ public class Asteroids : MonoBehaviour
         }
         
     }
-<<<<<<< Updated upstream
-=======
     void Update()
     {
         
@@ -47,38 +41,38 @@ public class Asteroids : MonoBehaviour
         if (collision.gameObject.CompareTag("Bullet"))
         {
             hits++;
+            Destroy(collision.gameObject);
             if (hits == 3 && asteroidType[0] == 'l')
             {
-                for(int i = 0; i < 2; i++){
-                    Instantiate(mediumAsteroidPrefabs[Random.Range(0, 5)], transform.position, transform.rotation);
-                }
                 OnDestroy();
-                gameManager.asteroidCount--;
+                for(int i = 0; i < 2; i++){
+                    Asteroids newAsteroid = Instantiate(mediumAsteroidPrefabs[Random.Range(0, 5)], transform.position, transform.rotation);
+                    newAsteroid.gameManager = gameManager;
+                }
                 Destroy(gameObject);
-                Destroy(collision.gameObject);
             }
             if (hits == 2 && asteroidType[0] == 'm')
             {
+                OnDestroy();
                 for(int i = 0; i < 2; i++){
-                    Instantiate(smallAsteroidPrefabs[Random.Range(0, 4)], transform.position, transform.rotation);
+                    Asteroids newAsteroid = Instantiate(smallAsteroidPrefabs[Random.Range(0, 4)], transform.position, transform.rotation);
+                    newAsteroid.gameManager = gameManager;
                 }
                 OnDestroy();
-                gameManager.asteroidCount--;
                 Destroy(gameObject);
-                Destroy(collision.gameObject);
             }
             if (hits == 1 && asteroidType[0] == 's')
             {
                 OnDestroy();
-                gameManager.asteroidCount--;
                 Destroy(gameObject);
-                Destroy(collision.gameObject);
             }
         }
     }
     void OnDestroy()
     {
+        if(gameManager != null){
+            gameManager.asteroidCount--;
+        }
         Instantiate(destroyFx, transform.position, transform.rotation); 
     }
->>>>>>> Stashed changes
 }
