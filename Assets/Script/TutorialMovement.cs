9c9624bf3d2742c68af36e3d9a02e180f5e129e5
Kim-Arcena <kvarcena@up.edu.ph>
2024-04-18 23:30:26 +0800

    add progression to keys

--- Assets/Script/TutorialMovement.cs
+++ Assets/Script/TutorialMovement.cs
@@ -8,57 +8,123 @@ public class TutorialMovement : MonoBehaviour
     [SerializeField] private float maxMoveSpeed = 10f;
     [SerializeField] private float rotateSpeed = 180f;
     [SerializeField] private float acceleration = 10f;
+    [SerializeField] GameObject rotateCanvas;
+    [SerializeField] GameObject arrowUPCanvas;
+    [SerializeField] GameObject shooterCanvas;
 
     [Header("Ship Components")]
     [SerializeField] private Transform spawnBullet;
     [SerializeField] private Rigidbody bulletPrefab;
     [SerializeField] private float bulletSpeed = 20f;
 
-
     private Rigidbody shipRb;
     private bool isAlive = true;
     private bool isAccelerating = false;
 
-    [Header ("Tuturial")]
+    [Header("Tutorial")]
     private bool rotateTutorial = true;
     private bool thrusterTutorial = false;
     private bool shooterTutorial = false;
     private bool destroyTutorial = false;
     private bool sceneOne = false;
     private bool largeAsteroidScene = false;
-    
 
-    private void Start(){
+    private bool leftArrowPressed = false;
+    private bool rightArrowPressed = false;
+    private bool topArrowPressed = false;
+    private bool spacebarPressed = false;
+    private float leftArrowPressTime = 0f;
+    private float rightArrowPressTime = 0f;
+    private float topArrowPressTime = 0f;
+    private int spaceBarPressedCounter = 0;
+
+    private void Start()
+    {
         shipRb = GetComponent<Rigidbody>();
-	
     }
+
     private void Update()
     {
         if (rotateTutorial)
         {
             ShipRotation();
-            acceleration = 0;
+
+            if (leftArrowPressed)
+                leftArrowPressTime += Time.deltaTime;
+
+            if (rightArrowPressed)
+                rightArrowPressTime += Time.deltaTime;
+
+            if (leftArrowPressTime >= 1f || rightArrowPressTime >= 1f)
+            {
+                thrusterTutorial = true;
+                acceleration = 50f;
+            }
+            arrowUPCanvas.SetActive(false);
+            shooterCanvas.SetActive(false);
+        }
+        
+        if(thrusterTutorial){
+            ShipRotation();
+            ShipAccelaration();
+
+            if (topArrowPressed)
+                topArrowPressTime += Time.deltaTime;
+            
+            if (topArrowPressTime >= 1f){
+                shooterTutorial = true;
+            }
+            rotateCanvas.SetActive(false);
+            arrowUPCanvas.SetActive(true);
+        }
+
+        if(shooterTutorial){
+            ShipRotation();
+            ShipAccelaration();
+            ShipShoot();
+            arrowUPCanvas.SetActive(false);
+            shooterCanvas.SetActive(true);
+        }
+
+
+    }
+
+    private void ShipRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            leftArrowPressed = true;
+        }
+        else
+        {
+            leftArrowPressed = false;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * -rotateSpeed * Time.deltaTime);
+            rightArrowPressed = true;
+        }
+        else
+        {
+            rightArrowPressed = false;
         }
     }
+
     private void ShipAccelaration()
     {
         if(Input.GetKey(KeyCode.UpArrow))
         {
             shipRb.AddForce(transform.up * acceleration);
             shipRb.velocity = Vector3.ClampMagnitude(shipRb.velocity, maxMoveSpeed);
+            topArrowPressed = true;
+        }
+        else{
+            topArrowPressed = false;
         }
     }
-    private void ShipRotation()
-    {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
-        }
-        else if(Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(Vector3.forward * -rotateSpeed * Time.deltaTime);
-        }
-    }
+
     private void ShipShoot()
     {
         if(Input.GetKeyDown(KeyCode.Space))
@@ -73,7 +139,10 @@ public class TutorialMovement : MonoBehaviour
             }
             bullet.velocity = shipSpeed * shipDirection;
             bullet.AddForce(spawnBullet.up * bulletSpeed, ForceMode.Impulse);
+            spacebarPressed = true;
+        }
+        else{
+            spacebarPressed = false;
         }
     }
-
 }
