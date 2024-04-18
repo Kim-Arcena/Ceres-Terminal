acfbffab0287a27c60c519ed9e20212f9dfb7870
Kim-Arcena <kvarcena@up.edu.ph>
2024-04-18 13:51:50 +0800

    initial chanegs to menu scene

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private string scenename;
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player")){
+            SceneManager.LoadScene(scenename);
+        }
+    }
+}
