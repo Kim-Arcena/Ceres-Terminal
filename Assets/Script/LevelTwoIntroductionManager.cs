1ed7afc0ed4ff470d7cad4f07b278e23f82a95bd
Divine <divinelavente@gmail.com>
2024-04-20 03:35:25 +0800

    fixed level two animation bug

--- Assets/Script/LevelTwoIntroductionManager.cs
+++ Assets/Script/LevelTwoIntroductionManager.cs
@@ -4,27 +4,62 @@ using UnityEngine;
 
 public class LevelTwoIntroductionManager : MonoBehaviour
 {
-    [SerializeField] public Transform shipTransform;
-    [SerializeField] public Vector3 endPosition = new Vector3(0f, 50.0f, -5f);
-
-    private LevelTwoDialogueManager dialogueManager;
+    [SerializeField] public GameObject shipPlayer;
+    [SerializeField] public GameObject FirstDialogue;
+    [SerializeField] public GameObject SecondDialogue;
+    [SerializeField] public bool moveShip = true;
+    [SerializeField] public bool firstEvent = false;
+    [SerializeField] public bool secondEvent = false;
+    [SerializeField] public float moveSpeed = 2f;
+    [SerializeField] public Vector3 firstPosition = new Vector3(0f, 50.0f, -1.5f);
+    [SerializeField] public Vector3 secondPosition = new Vector3(0f, 50.0f, 5f);
     public float speed = 5f;
 
     void Start()
     {
-        dialogueManager = FindObjectOfType<LevelTwoDialogueManager>();
     }
 
     void Update()
     {
-        if (dialogueManager.finishDialogue)
+        if (moveShip && firstEvent == false)
         {
-            shipTransform.Translate((endPosition + shipTransform.position).normalized * speed * Time.deltaTime);
-
-            if (Vector3.Distance(shipTransform.position, endPosition) < 0.1f)
+            shipPlayer.transform.Translate((firstPosition + shipPlayer.transform.position).normalized * moveSpeed * Time.deltaTime);
+            Debug.Log(Vector3.Distance(shipPlayer.transform.position, firstPosition));
+            if (Vector3.Distance(shipPlayer.transform.position, firstPosition) < 0.25f)
             {
-                shipTransform.position = endPosition;
+                shipPlayer.transform.position = firstPosition;
+                moveShip = false;
+                firstEvent = true;
+                FirstDialogue.SetActive(true);
+            }
+        }
+
+        if(moveShip == false && firstEvent)
+        {
+            Invoke("StartSecondEvent", 7.5f);
+        }
+
+        if(moveShip && secondEvent)
+        {
+            shipPlayer.transform.Translate((secondPosition + shipPlayer.transform.position).normalized * moveSpeed * Time.deltaTime);
+            Debug.Log(Vector3.Distance(shipPlayer.transform.position, secondPosition));
+            if (Vector3.Distance(shipPlayer.transform.position, secondPosition) < 0.25f)
+            {
+                shipPlayer.transform.position = secondPosition;
             }
         }
     }
+
+    void StartSecondEvent()
+    {
+        SecondDialogue.SetActive(true);
+        secondEvent = true;
+        Invoke("MoveShip", 5f);
+    }
+
+    void MoveShip()
+    {
+        moveShip = true;
+    }
+
 }
