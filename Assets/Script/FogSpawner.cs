2d5c5709e81379af4bce2ec53c9ef50f8e016df6
Kim-Arcena <kvarcena@up.edu.ph>
2024-04-20 13:37:09 +0800

    finalization of the unity app build

--- Assets/Script/FogSpawner.cs
+++ Assets/Script/FogSpawner.cs
@@ -43,7 +43,7 @@ public class FogSpawner : MonoBehaviour
 
     void StartSpawning(){
         StartCoroutine(SpawnAsteroidsWithDelay());
-    }
+    }   
 
     private IEnumerator SpawnAsteroidsWithDelay()
     {
