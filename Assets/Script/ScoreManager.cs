26ff531482795bc52d698d0bbc96a8bf1f30cb39
aldrich <amtoreres@up.edu.ph>
2024-05-29 22:38:53 +0800

    add scoring system

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+
+    private void Awake()
+    {
+        // Singleton pattern to ensure only one instance of ScoreManager exists
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        UpdateScoreText();
+    }
+
+    public void SetScoreText(TextMeshProUGUI newScoreText)
+    {
+        scoreText = newScoreText;
+        UpdateScoreText();
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+}
